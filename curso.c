10f8dc0e71b667de392eab5519f873fdb8c3aee2
Tata <112900054+JanieliSilva@users.noreply.github.com>
2023-04-28 12:50:28 +0000

    inicio do curso.c

--- curso.c
+++ curso.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include"aluno.h"
 
+typedef struct curso{
+    int codigo;
+    Aluno *lista_de_alunos;
+    char nome[50];
+    int vagas;
+    int matriculados;
+}Curso;

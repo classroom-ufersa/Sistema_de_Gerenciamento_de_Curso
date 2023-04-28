82eb628cdd0675740a84cff9d72ec1587bd5412a
Tata <112900054+JanieliSilva@users.noreply.github.com>
2023-04-28 12:47:54 +0000

    Começando

--- aluno.c
+++ aluno.c
@@ -1,1 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+  struct aluno{
+    char nome[100];
+    int matricula;
+    char curso;
+    float notas;
+	struct alunos *next;
+};
--- aluno.h
+++ aluno.h
@@ -1,1 +1,9 @@
+#ifndef ALUNOS_H
+#define ALUNOS_H
+#include <string.h>
+#include "aluno.c"
 
+typedef struct aluno Aluno;
+
+
+#endif

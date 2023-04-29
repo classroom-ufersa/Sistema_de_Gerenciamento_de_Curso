996da22a61cdcdfa8ca4c334de56e439bd9c370c
Ana Santana <123104984+Anaasca07@users.noreply.github.com>
2023-04-29 19:03:38 +0000

    função

--- curso.c
+++ curso.c
@@ -10,3 +10,28 @@ typedef struct curso{
     int vagas;
     int matriculados;
 }Curso;
+
+ Aluno *realizar_matricula(void){
+	  	
+		   Aluno *alunos = (Aluno*)malloc(sizeof(Aluno));
+		   
+		   printf("digite seu nome\n");
+		   scanf(" %[^\n]", alunos->nome);
+		   printf("\n");
+		   
+		   printf("digite sua matricula\n");
+		   scanf("%d" , &alunos->matricula);
+		   printf("\n");
+
+           printf("digite seu curso\n");
+		   scanf(" %[^\n]", &alunos->curso);
+		   printf("\n"); 
+		   
+		   printf("digite sua nota\n");
+		   scanf("%f" , &alunos->notas);
+	       printf("\n");
+        FILE* arq = fopen ("matricula.txt", "w");
+	  	 if(arq ==NULL){
+        printf("Erro ao abrir o arquivo: ");
+        return 1; 
+ }

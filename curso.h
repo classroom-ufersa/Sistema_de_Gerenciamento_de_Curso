ed48871b803b14267d634e77c8fc57287b4ae5fd
Tata <112900054+JanieliSilva@users.noreply.github.com>
2023-05-16 16:52:41 -0300

    adicionando

--- curso.h
+++ curso.h
@@ -3,6 +3,24 @@
 #include <string.h>
 #include "curso.c"
 
-typedef struct curso Curso;
+typedef struct curso;
 
-#endif
+typedef struct list list;
+
+void LimpaBuffer();
+
+void atualizarArquivo(curso *c);
+
+Aluno *realizar_matricula(void);
+
+curso* capturarListaAlunos(curso* c);
+
+void inserir_matricula (Aluno *novo_aluno, curso *curso);
+
+int remover(int mat, curso *c);
+
+Aluno* busca(int mat, curso* c);
+
+int imprime (curso *c);
+
+int editar(curso *c);

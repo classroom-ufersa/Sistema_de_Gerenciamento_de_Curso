7a51024721877f89ab9973bc6a46c415dfabfc70
Tata <112900054+JanieliSilva@users.noreply.github.com>
2023-05-16 16:54:23 -0300

    adicionando funções

--- curso.c
+++ curso.c
@@ -74,4 +74,80 @@ typedef struct list{
     fclose(arq);
     return alunos;
 	}
+curso* capturarListaAlunos(curso* c) {
+    FILE* arq = fopen("ListadaMatricua.txt", "r");
+    if (arq == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+    } else {
+        Aluno* aluno = NULL;
+        while (1) {
+            aluno = (Aluno*)malloc(sizeof(Aluno));
+            if (aluno == NULL) {
+                printf("Erro ao alocar memoria.\n");
+                break;
+            }
+            int result = fscanf(arq, "INFORMACOES DO MATRICULADO\nNome: %99[^\n]\nMatricula: %d\nNota: %f\n\n", aluno->nome, &aluno->matricula, &aluno->nota);
+            if (result != 3) {
+                free(aluno);
+                break;
+            }
+            aluno->next = NULL;
+            if (c->lista_de_alunos == NULL) {
+                c->lista_de_alunos = aluno;
+            } else {
+                aluno->next = c->lista_de_alunos;
+                c->lista_de_alunos = aluno;
+            }
+        }
+        fclose(arq);
+    }
+    //Inserir algoritmo de ordenção aqui 
+
+    return c;
+   
+}
+
+    void inserir_matricula (Aluno *novo_aluno, curso *curso){
+            
+            if(curso->vagas != 0){
+                           
+            if(curso->lista_de_alunos == NULL){
+                curso->lista_de_alunos = novo_aluno;
+                curso->lista_de_alunos->next = NULL;
+                curso->matriculados++;
+                curso->vagas--;
+            } else if(curso->lista_de_alunos != NULL){
+                novo_aluno->next = curso->lista_de_alunos;
+                curso->lista_de_alunos = novo_aluno;
+                curso->vagas--;
+                }             
+            
+            } else {
+                 printf("\nNao ha mais vagas no curso de %s", curso->nome);  
+         }
+    }
+    
+ int remover(int mat, curso *c){
+    c = capturarListaAlunos(c);
+    Aluno *alunoRemovido = c->lista_de_alunos;
+    Aluno *aux = NULL;
+
+    if(alunoRemovido->matricula == mat){
+         c->lista_de_alunos = alunoRemovido->next;
+         free(alunoRemovido);
+         atualizarArquivo(c);
+         return 1;
+    }
+
+    for(alunoRemovido = c->lista_de_alunos; alunoRemovido != NULL; alunoRemovido = alunoRemovido->next){    
+            if(alunoRemovido->matricula == mat){
+               aux->next = alunoRemovido->next;
+               free(alunoRemovido);
+               atualizarArquivo(c);
+               break;
+            }
+            aux = alunoRemovido; 
+       } 
+      
+} 
 
